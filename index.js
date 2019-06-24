b697246f9797656315c8117634554a06256796f7
Danny Harpigny <danharp.contact@gmail.com>
2019-06-24 21:57:47 +0200

    Initial bot

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs      = require('fs')
+const YAML    = require('js-yaml')
+const { ShardingManager } = require('discord.js')
+
+const cfg     = YAML.safeLoad(fs.readFileSync('./settings.yml'))
+const manager = new ShardingManager('./shard.js', { token: cfg.discord.token })
+
+manager.on('launch', (shard) => console.log(`Launching shard ${shard.id + 1}`))
+
+manager.spawn()
